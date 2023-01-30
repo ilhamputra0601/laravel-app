411ab592f7854969316fb38ec778aa2398ad7e04
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-01-30 20:51:34 +0700

    wwww

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -10,16 +10,28 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
         @endif
+        @if (session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        @endif
         <main class="form-signin w-100 m-auto">
             <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form>
+            <form action="/login" method="post" >
+                @csrf
               <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="name@example.com" autofocus value="{{ old('name') }}">
+                <label for="email">Email address</label>
+                @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
               </div>
               <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
+                <label for="password">Password</label>
               </div>
               <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +18,19 @@ use App\Http\Controllers\RegisterController;
 */
 
 Route::get('/',[PostController::class,'home']);
-
 Route::get('/about',[PostController::class,'about']);
 
 Route::get('/blog',[PostController::class,'index']);
-
 Route::get('/blog/{post:slug}',[PostController::class,'show']);
-
 
 Route::get('/categories',[CategoryController::class,'index']);
 
-Route::get('/login',[LoginController::class,'index']);
-Route::get('/register',[RegisterController::class,'index']);
+Route::get('/login',[LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('/login',[LoginController::class,'authenticate']);
+Route::post('/logout',[LoginController::class,'logout']);
+
+Route::get('/register',[RegisterController::class,'index'])->middleware('guest');
 Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/dashboard',[DashboardController::class,'index'])->middleware('auth');
 
