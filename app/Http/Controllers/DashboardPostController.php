c9f589753e0b17bd674f82ee14214d4aa22978f1
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-02-04 02:54:54 +0700

    crud passed

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 
 class DashboardPostController extends Controller
@@ -42,7 +43,23 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        // return $request;
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required',
+        ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body),200,'...');
+        // striptags untuk menghilangkan tanda html
+
+        Post::create($validatedData);
+
+        return redirect('/dashboard/posts')->with('success','New post has been added!');
+
     }
 
     /**
@@ -66,7 +83,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view ('dashboard.posts.edit',[
+            'post' => $post,
+            'Categories' => Category::all()
+        ]);
     }
 
     /**
@@ -78,7 +98,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required',
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+        $validatedData = $request->validate($rules);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body),200,'...');
+        // striptags untuk menghilangkan tanda html
+
+        Post::where('id', $post->id)
+            ->update($validatedData);
+
+        return redirect('/dashboard/posts')->with('success','post has been updated!');
     }
 
     /**
@@ -89,7 +127,8 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+        return redirect('/dashboard/posts')->with('success','Post has been Deleted!');
     }
 
     public function checkSlug(Request $request)
