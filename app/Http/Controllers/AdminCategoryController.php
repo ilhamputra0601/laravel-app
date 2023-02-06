ca8ecb38680bdb44180cd7dc2ddd8eeca81a1654
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-02-07 05:24:15 +0700

    ss

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -21,7 +21,7 @@ class AdminCategoryController extends Controller
         // }
         // massuk ke app service provider untuk nambah gates
 
-        // untuk cek gates nnti coba untuk apus admin.php
+        // untuk cek gates nnti coba untuk apus admin.php 
         // $this->authorize('admin');
 
         return view('dashboard.categories.index',[
